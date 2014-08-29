ae94e26bf217e4b7ab7e16425d51e6808a67ebf3
宇宙総姐夫 <xuechong87@gmail.com>
2014-08-29 10:48:46 +0800

    Update sqlToStringBuilder.py
    fix 引号

--- sqlToStringBuilder.py
+++ sqlToStringBuilder.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     path = ''
     open(path)
     result = 'StringBuilder sql = new StringBuilder();\n'
-    warp = lambda x :'sql.append("' + x.replace('"', '\"').replace('\n',' ') + '");\n'
+    warp = lambda x :'sql.append("' + x.replace('"', '\\"').replace('\n',' ') + '");\n'
     for line in open(path).readlines():
         result = result + warp(line)
     print (result)
