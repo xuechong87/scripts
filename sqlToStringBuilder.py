51a7c0f8b191642f57b091cf1ca5eb1ae400cf7f
宇宙総姐夫 <xuechong87@gmail.com>
2014-08-29 10:46:56 +0800

    Create sqlToStringBuilder.py

---
+++ sqlToStringBuilder.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+'''
+Created on 2014年8月29日
+@author: xuechong
+'''
+
+if __name__ == '__main__':
+    path = ''
+    open(path)
+    result = 'StringBuilder sql = new StringBuilder();\n'
+    warp = lambda x :'sql.append("' + x.replace('"', '\"').replace('\n',' ') + '");\n'
+    for line in open(path).readlines():
+        result = result + warp(line)
+    print (result)
