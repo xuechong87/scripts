78c33df1a9bfa659f85fe2241c0f37c231c8a0c2
宇宙総姐夫 <xuechong87@gmail.com>
2015-04-07 09:47:14 +0800

    Update sqlToStringBuilder.py

--- sqlToStringBuilder.py
+++ sqlToStringBuilder.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     open(path)
     result = 'StringBuilder sql = new StringBuilder();\n'
     warp = lambda x :'sql.append("' + x.replace('"', '\\"').replace('\n',' ') + '");\n'
-    for line in open(path).readlines():
-        result = result + warp(line)
+    # for line in open(path).readlines():
+    #    result = result + warp(line)
+    result = reduce(lambda r,c : r + warp(c),open(path).readlines(),result)
     print (result)
